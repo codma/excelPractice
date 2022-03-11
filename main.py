8342c259cb8f4a6ac7b3bd591b9769d317c32038
Sydney <jinjoolee.0319@gmil.com>
2022-03-11 17:14:19 +0900

    워크시트 불러오기 및 데이터 수정 연습

--- main.py
+++ main.py
@@ -2,17 +2,40 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 
 # 엑셀파일 쓰기
-write_wb = Workbook()
+# write_wb = Workbook()
 # 이름이 있는 시트를 생성
-write_ws = write_wb.create_sheet('생성시트')
-write_wb.save("/Users/jinjoolee/Desktop/Project/python/sample.xlsx")
+# write_ws = write_wb.create_sheet('생성시트')
+# write_wb.save("/Users/jinjoolee/Desktop/Project/python/sample.xlsx")
 
 # 입력 자리를 지정
-write_ws.cell(1,1,'연습해보자')
+# write_ws.cell(1,1,'연습해보자')
+
+# 파일 불러오기
+load_wb = load_workbook("/Users/jinjoolee/Desktop/Project/python/sample.xlsx")
+load_ws = load_wb['생성시트']
+
+# 셀 주소로 값 출력
+all_values = []
+for row in load_ws.rows:
+    row_value = []
+    for cell in row:
+        row_value.append(cell.value)
+    all_values.append(row_value)
+print(all_values)
 
 
-write_wb.save("/Users/jinjoolee/Desktop/Project/python/sample.xlsx")
+rowCount = 2
+columnCount = 1
+
+for row in all_values:
+    for value in row:
+        load_ws.cell(rowCount, columnCount, value)
+        if rowCount == 2 and columnCount == 1:
+            load_ws.cell(rowCount, columnCount, '깜짝등장')
+        columnCount = columnCount + 1
+    rowCount = rowCount + 1
+    columnCount = 1
 
 
-# 과제: 최소 20개 컬럼에 대해서 최소 10000개 로우를 가져와서 합치기 응용하기!
-# 다음주 수요일에 1차 피드백, 그 다음 월요일 수업
+load_wb.save("/Users/jinjoolee/Desktop/Project/python/sample.xlsx")
+
